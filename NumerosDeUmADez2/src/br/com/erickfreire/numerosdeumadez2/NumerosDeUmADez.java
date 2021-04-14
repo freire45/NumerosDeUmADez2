4b16d31d87d68650c7fc05549a5217b196832ede
Erick Freire <erilefr@gmail.com>
2021-04-14 18:01:57 -0300

    NumerosDeUmADez2
    NumerosDeUmADez2

---
+++ NumerosDeUmADez2/src/br/com/erickfreire/numerosdeumadez2/NumerosDeUmADez.java
@@ -0,0 +1,18 @@
+package br.com.erickfreire.numerosdeumadez2;
+
+/**
+ * Programa em Java que mostra os valores de 1 a 10 com laço for
+ * @author Erick Freire
+ * @version 1 - Criado em 14-04-2021 as 17:56
+ */
+
+public class NumerosDeUmADez {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 10; i++) {
+			System.out.printf("%d ", i);
+		}
+		
+		System.out.println();
+	}
+
+}
